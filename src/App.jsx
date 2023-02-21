9eb115c672f939528b37ca339500cb2e9a01e385
Kyryl <kyryl.zp@gmail.com>
2023-02-21 19:04:48 +0200

    Create App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+export const App = () => {
+
+  return (
+      <>
+          <header></header>
+          <main>
+              <nav></nav>
+              <section>1111</section>
+          </main>
+          <footer></footer>
+      </>
+  )
+};
